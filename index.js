9fc4aad006f73230dd93288e33b2be17a2fb5476
EwyynTomato <nkaloli@gmail.com>
2015-06-04 17:28:11 +0800

    remove long-compile time react

--- index.js
+++ index.js
@@ -8,8 +8,6 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var React = require("react/addons");
-var ReactBootstrap = require("react-bootstrap");
 var joinClasses = require("react/lib/joinClasses");
 var cx = require("classnames");
 
